24855e68b64a3e30fe25a9676aca1da875ccee89
Lpruneau24 <80844337+Lpruneau24@users.noreply.github.com>
2024-05-21 11:00:15 -0400

    Update Dominos.py

--- Dominos.py
+++ Dominos.py
@@ -1,11 +1,11 @@
-def getPizzaDict (filePath):
+def getPizzaDict (filePath):    # This function will create dictionaries with crust types as keys and prices as values nested inside a bigger dictionary with sizes as keys.
     import collections
 
     pizza = open(filePath, 'r')
     pizza_list = pizza.readlines()
     pizza_dict = collections.defaultdict(dict)
 
-    for row in pizza_list[1:]: #evaluates all the lines with a variable called row (not including header line 0)
+    for row in pizza_list[1:]: # evaluates all the lines with a variable called row (not including header line 0)
         vals = row.strip().lower().split()
         print(vals)
         if vals[0] in {'small', 'medium', 'large', 'xlarge'}:
@@ -13,7 +13,7 @@ def getPizzaDict (filePath):
 
     pizza.close
     return pizza_dict
-def orderEntry():
+def orderEntry():    # This function will ask the user what kind of pizza they want and using the dictionary above, return a list of 3 items being size, crust and price. This will be called many times.
 
     size = input("What size would you like? [small/medium/large/xlarge]")
     while size not in {'small','medium','large','xlarge'}:
@@ -58,33 +58,32 @@ def orderEntry():
         
     return [size, crust, price]
 
-class Tax:
+class Tax:   # The class created here will calculate taxes. I have set the default taxRate to 0.13 which doesn't matter because it is called as a parameter anyway
     def __init__(self, subtotal, taxRate = 0.13):
         self.subtotal = subtotal
         self.taxRate = taxRate
-    def get_tax(self):
+    def get_tax(self):    # This will return the sales tax of the pizza entered in orderEntry()
         salesTax = float(subtotal) * float(self.taxRate)
         return salesTax
-    def get_total(self):
+    def get_total(self):    # This will return the after tax total of the pizza entered in orderEntry()
         total = float(subtotal) + float(subtotal) * float(self.taxRate)
         return total
 
-pizzaFile = "D:\Leandre\Apps\GitHub\Repository\Dominos\Pizza.txt" # input("Paste the file path for pizzas please.")
+pizzaFile = input("Paste the file path for pizzas please.")
 pizza_dict = getPizzaDict(pizzaFile)
-
+# This part will take the keys of the different nested dictionaries from getPizzaDict()
 small_keys = pizza_dict['small'].keys()
 medium_keys = pizza_dict['medium'].keys()
 large_keys = pizza_dict['large'].keys()
 xlarge_keys = pizza_dict['xlarge'].keys()
 
-taxRate = 0.13 # input("What is the tax rate?")
+taxRate = input("What is the tax rate?")
 
 item_list = orderEntry()
-
 subtotal = 0
 subtotal = subtotal + float(item_list[2])
 print(f"Your subtotal is ${subtotal}")
-
+#T his will loop the ordering process for as many items as the customer wants. Subtotal is an accumulator value and taxes are done on that value once customer is done ordering new items.
 while True:
     order_again = input("Would you like to order something else? [yes/no]")
     while order_again not in {'yes', 'no'}:
